636fdc8e76a9d2a67de248a12dd00d4e4ddaee34
FekadeTegeghe <67452800+FekadeTegeghe@users.noreply.github.com>
2021-02-23 07:16:09 -0500

    Update q5.js

--- w1d2_assignment/q5.js
+++ w1d2_assignment/q5.js
@@ -1,7 +1,7 @@
 let lowestHeartBeatRate;
 let fastestHeartBeatRate;
 let age = prompt('enter your age');
-let maxHeartBeat = 200 - age;
+let maxHeartBeat = 220 - age;
 fastestHeartBeatRate = 0.85*maxHeartBeat;
 console.log(fastestHeartBeatRate);
 lowestHeartBeatRate = 0.65*maxHeartBeat;
