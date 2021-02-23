a87f540e6d41925c8e77fa52facfc416780e924a
Sosi <fekade.1979@gmail.com>
2021-02-22 20:00:33 -0500

    Create lastTest.js

---
+++ lastTest.js
@@ -0,0 +1,8 @@
+let q1 = 'where are you from?';
+console.log(q1);
+let answer = 'I am from gion';
+console.log(answer);
+let origin = prompt("where are you from?");
+if(origin ==='Ethiopia'){
+    console.log('You are awesome!');
+}
