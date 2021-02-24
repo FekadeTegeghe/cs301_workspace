aaaa32acd9cfebbe6c9221bbd7be20b0bc0a8047
Sosi <fekade.1979@gmail.com>
2021-02-23 22:06:07 -0500

    edited

--- w1d2_assignment/q4.js
+++ w1d2_assignment/q4.js
@@ -4,4 +4,4 @@ let beginOdometer = +prompt('enter beginning odometer reading: ');
 let endOdometer = +prompt('enter ending odometer reading: ');
 let gasUsed = +prompt('enter used gas in gallons: ');
 milesPerGallon = (endOdometer - beginOdometer)/gasUsed;
-console.log(milesPerGallon);
+console.log(`The number of miles the car moves per gallon of feul is: ${milesPerGallon}`);
