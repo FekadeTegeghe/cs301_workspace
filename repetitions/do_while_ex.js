4c2b358860fe1c6b42957725491728183e4a7850
Sosi <fekade.1979@gmail.com>
2021-02-25 18:38:41 -0500

    assignment Completed

---
+++ repetitions/do_while_ex.js
@@ -0,0 +1,6 @@
+//Do while
+let k = 0;
+do{
+    console.log(i);
+    i++;
+}while(i<3);
---
+++ repetitions/while_ex1.js
@@ -0,0 +1,11 @@
+let i = 0;
+while(i<3){
+    console.log(i);
+    i++;
+}
+//while(i)<=> while(i!=0)
+let j = 3;
+while(i){
+    console.log(i);
+    i--;
+}
